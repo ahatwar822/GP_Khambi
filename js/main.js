377597ee8f4e4fc1a41dc035836360b3aa4208ac
ahatwar822 <himanshugaupale875@gmail.com>
2025-10-14 13:52:53 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,130 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('fixed-top').css('padding', '0');
+        } else {
+            $('.nav-bar').removeClass('fixed-top').css('padding', '0px 90px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Donation progress
+    $('.donation-item .donation-progress').waypoint(function () {
+        $('.donation-item .progress .progress-bar').each(function () {
+            $(this).css("height", $(this).attr("aria-valuenow") + '%');
+        });
+    }, {offset: '80%'});
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        animateOut: 'rotateOutUpRight',
+        animateIn: 'rotateInDownLeft',
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1000,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 1000,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    
+})(jQuery);
+
+
+// assets/js/header-loader.js
+(function () {
+  const fragments = [
+    { id: 'topbar-placeholder', url: '/fragments/topbar.html' },
+    { id: 'logo-placeholder', url: '/fragments/logo-strip.html' },
+    { id: 'navbar-placeholder', url: '/fragments/navbar.html' }
+  ];
+
+  fragments.forEach(f => {
+    const el = document.getElementById(f.id);
+    if (!el) return;
+    fetch(f.url, { cache: "no-cache" })
+      .then(r => { if (!r.ok) throw new Error('Network'); return r.text(); })
+      .then(html => el.innerHTML = html)
+      .catch(err => console.error('Error loading', f.url, err));
+  });
+})();
+
